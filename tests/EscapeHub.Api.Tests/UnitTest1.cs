9094eb1caad45527b6feaa752b481ed0b420d17c
Bikali Balázs <bikalibalazs04@gmail.com>
2026-08-30 18:55:33 +0200

    initial commit

---
+++ tests/EscapeHub.Api.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EscapeHub.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/EscapeHub.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EscapeHub.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
